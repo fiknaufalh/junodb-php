c0152a30bfa8bc9776a5fd609eadfdab92e14340
fiknaufalh <18221096@mahasiswa.itb.ac.id>
2024-07-16 18:25:18 +0700

    feat: implement simple php client

--- www/index.php
+++ www/index.php
@@ -10,34 +10,42 @@
     <?php
     require 'junodb.php';
 
-    $namespace = 'test_ns';
-    $key = 'test_key';
-
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $key = $_POST['key'];
+
         if (isset($_POST['create'])) {
             $value = $_POST['value'];
-            $response = create_data($namespace, $key, $value);
+            $response = create_data($key, $value);
             echo "<p>Create Response: " . json_encode($response) . "</p>";
         } elseif (isset($_POST['read'])) {
-            $response = read_data($namespace, $key);
+            $response = read_data($key);
             echo "<p>Read Response: " . json_encode($response) . "</p>";
         } elseif (isset($_POST['update'])) {
             $value = $_POST['value'];
-            $response = update_data($namespace, $key, $value);
+            $response = update_data($key, $value);
             echo "<p>Update Response: " . json_encode($response) . "</p>";
+        } elseif (isset($_POST['set'])) {
+            $value = $_POST['value'];
+            $response = set_data($key, $value);
+            echo "<p>Set Response: " . json_encode($response) . "</p>";
         } elseif (isset($_POST['delete'])) {
-            $response = delete_data($namespace, $key);
+            $response = delete_data($key);
             echo "<p>Delete Response: " . json_encode($response) . "</p>";
         }
     }
     ?>
 
     <form method="post">
+        <label for="key">Key:</label>
+        <input type="text" id="key" name="key" required>
+        <br>
         <label for="value">Value:</label>
-        <input type="text" id="value" name="value" required>
+        <input type="text" id="value" name="value">
+        <br>
         <button type="submit" name="create">Create</button>
         <button type="submit" name="read">Read</button>
         <button type="submit" name="update">Update</button>
+        <button type="submit" name="set">Set</button>
         <button type="submit" name="delete">Delete</button>
     </form>
 </body>
--- www/junodb.php
+++ www/junodb.php
@@ -5,36 +5,54 @@ function junodb_request($method, $url, $data = null) {
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
 
     if ($data) {
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded']);
     }
 
     $response = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        $error_msg = curl_error($ch);
+        curl_close($ch);
+        return ['error' => $error_msg];
+    }
+
     curl_close($ch);
 
-    return json_decode($response, true);
+    $decoded_response = json_decode($response, true);
+
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        return ['error' => json_last_error_msg(), 'response' => $response];
+    }
+
+    return $decoded_response;
 }
 
-function create_data($namespace, $key, $value) {
-    $url = 'http://proxy:8080/create';
-    $data = ['namespace' => $namespace, 'key' => $key, 'value' => $value];
+function create_data($key, $value) {
+    $url = 'http://10.181.133.164:8181/samplejuno/recordcreate';
+    $data = ['key' => $key, 'value' => $value];
     return junodb_request('POST', $url, $data);
 }
 
-function read_data($namespace, $key) {
-    $url = 'http://proxy:8080/get?namespace=' . urlencode($namespace) . '&key=' . urlencode($key);
+function read_data($key) {
+    $url = 'http://10.181.133.164:8181/samplejuno/reactget/' . urlencode($key);
     return junodb_request('GET', $url);
 }
 
-function update_data($namespace, $key, $value) {
-    $url = 'http://proxy:8080/update';
-    $data = ['namespace' => $namespace, 'key' => $key, 'value' => $value];
+function update_data($key, $value) {
+    $url = 'http://10.181.133.164:8181/samplejuno/reactset/';
+    $data = ['key' => $key, 'value' => $value];
     return junodb_request('POST', $url, $data);
 }
 
-function delete_data($namespace, $key) {
-    $url = 'http://proxy:8080/destroy';
-    $data = ['namespace' => $namespace, 'key' => $key];
+function set_data($key, $value) {
+    $url = 'http://10.181.133.164:8181/samplejuno/reactupdate/';
+    $data = ['key' => $key, 'value' => $value];
     return junodb_request('POST', $url, $data);
+}
+
+function delete_data($key) {
+    $url = 'http://10.181.133.164:8181/samplejuno/reactdelete/' . urlencode($key);
+    return junodb_request('DELETE', $url);
 }
 ?>
